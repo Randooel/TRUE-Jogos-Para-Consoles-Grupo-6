6cbde2cf5c9cb083fe530d148080a6ad8eca0c1f
Randooel <114538289+Randooel@users.noreply.github.com>
2024-03-31 14:40:26 -0300

    MainBranch changelog v.0.01
    - Alteração função Print, linha 10

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿// See https://aka.ms/new-console-template for more information
 private void main()
 {
     Print()
+    Console.WriteLine("Tchau, carinha!")
 }
 
 public void Print()
 {
-    Console.WriteLine("Hello, carinha!");
+    Console.WriteLine("Hello, carinha!!!!!!!!!!!!");
 }
