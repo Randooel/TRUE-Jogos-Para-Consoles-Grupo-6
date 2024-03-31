3a4f9394ced821ec3bc2df06f75f19b5e17c5c76
Randooel <114538289+Randooel@users.noreply.github.com>
2024-03-31 14:51:00 -0300

    changelog v0.02
    - Alteração da função Print, com adição de "Hello, Carinaldo"

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿// See https://aka.ms/new-console-template for more information
 private void main()
 {
     Print()
-    Console.WriteLine("Tchau, carinaldo!");
+    Console.WriteLine("Tchau, carinha!")
 }
 
 public void Print()
 {
+    Console.WriteLine("Hello, carinha!!!!!!!!!!!!");
     Console.WriteLine("Hello, carinaldo!");
 }
